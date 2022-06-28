0da1b2e9adf4b37177fe95259c4bf8ff8409c606
babydev-by03KOREA <hyeongju1122@gmail.com>
2022-06-28 17:17:21 +0900

    Setting App Clone Coding - ProfileCell, MenuCell

---
+++ Setting_Clone/ProfileCell.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var topTitle: UILabel!
+    
+    @IBOutlet weak var bottomDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let profileHeight: CGFloat = 60
+        profileImageView.layer.cornerRadius = profileHeight / 2
+        topTitle.textColor = .blue
+        topTitle.font = UIFont.systemFont(ofSize: 20)
+        
+        bottomDescription.textColor = .orange
+        bottomDescription.font = UIFont.systemFont(ofSize: 16)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+    
+}
--- Setting_Clone/ViewController.swift
+++ Setting_Clone/ViewController.swift
@@ -1,19 +1,59 @@
-//
-//  ViewController.swift
-//  Setting_Clone
-//
-//  Created by 박형주 on 2022/06/28.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var settingTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+        
+//      TableView 사용규칙
+//      let nib = UINib(nibName: "ProfileCell", bundle: nil)
+        
+        settingTableView.register(UINib(nibName: "ProfileCell", bundle: nil), forCellReuseIdentifier: "ProfileCell")
+//      nibName: File이름
+        settingTableView.register(UINib(nibName: "MenuCell", bundle: nil), forCellReuseIdentifier: "MenuCell")
     }
-
+    
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+//      갯수를 리턴한다
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == 0 {
+            //      어떤걸 보여줄거니?
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath)
+                    
+                    return cell
+            //      무조건 이 cell만 나타내주겠다!
+        }
+        
+        //      어떤걸 보여줄거니?
+                let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+                
+                return cell
+        //      무조건 이 cell만 나타내주겠다!
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+//          첫번째꺼는 내가 지정한대로 보여주고,
+            return UITableView.automaticDimension
+        }
+//      나머지는 그냥 60정도로만 줘
+        return 60
+    }
+    
+}
