7b272afbfe7ecdd492ddf32a57691b60996f56b2
babydev-by03KOREA <hyeongju1122@gmail.com>
2022-06-28 13:17:17 +0900

    Initial Commit

---
+++ Setting_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setting_Clone
+//
+//  Created by 박형주 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
